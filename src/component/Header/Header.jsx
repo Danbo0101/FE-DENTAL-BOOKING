8cd0691bda1f88705e030cb4fe89fe789c8742e0
Danbo0101 <danbo0101@gmail.com>
2024-11-11 15:43:10 -0500

    homepage

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,107 +1,109 @@
-import * as React from 'react';
-import { Dialog, DialogPanel } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon, } from '@heroicons/react/24/outline';
-import { useState } from 'react';
-import logo from '../../assets/images/D Dental.png'
-import { useDispatch, useSelector } from 'react-redux';
-import { AppBar } from '@mui/material';
-import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
-import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
-import HistoryOutlinedIcon from '@mui/icons-material/HistoryOutlined';
-import { postLogout } from '../../services/authService';
-import { useNavigate } from 'react-router-dom';
-import { doLogout } from '../../redux/action/userAction';
-import Profile from './Modal/Profile';
-import History from './Modal/History';
-import Setting from './Modal/Setting';
+import * as React from "react";
+import logo from "../../assets/images/D Dental.png";
+import Button from "@mui/material/Button";
+import { styled, alpha } from "@mui/material/styles";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
 
-
-
-import { styled, alpha } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-
-
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: "20px",
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
-    },
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: "20px",
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'white',
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: "white",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'white',
-    width: '100%',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        [theme.breakpoints.up('sm')]: {
-            width: '12ch',
-            '&:focus': {
-                width: '20ch',
-            },
-        },
+  color: "white",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
     },
+  },
 }));
 
-
-
-
 const Header = (props) => {
-    return (
-        <div className='flex px-6 mx-10 mt-4 rounded-md drop-shadow bg-sky-300 
-            justify-between items-center'>
-            <div div className='p-5' >
-                <img src={logo} className='w-28 h-14' />
-            </div >
-            <Search>
-                <SearchIconWrapper>
-                    <SearchIcon />
-                </SearchIconWrapper>
-                <StyledInputBase
-                    placeholder="Search…"
-                    inputProps={{ 'aria-label': 'search' }}
-                />
-            </Search>
-        </div >
-
-
-
-
-    )
-}
-
+  return (
+    <div
+      className="flex px-6 mx-10 mt-4 rounded-md drop-shadow bg-sky-300 
+            justify-between items-center"
+    >
+      <div div className="p-5">
+        <img src={logo} className="w-36 h-16" />
+      </div>
+      <div className="flex gap-32 mr-10">
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ "aria-label": "search" }}
+          />
+        </Search>
+        <div className="flex gap-3">
+          <Button
+            variant="text"
+            href="#outlined-buttons"
+            sx={{
+              fontSize: "14px",
+              fontFamily: "Roboto Slab, serif",
+              fontWeight: "600",
+              color: "black",
+              borderRadius: "10px",
+              "&:hover": {
+                color: "white",
+                background: "#2576d0",
+              },
+            }}
+          >
+            Login
+          </Button>
+          <Button
+            variant="outlined"
+            href="#outlined-buttons"
+            sx={{
+              color: "white",
+              fontSize: "14px",
+              fontFamily: "Roboto Slab, serif",
+              fontWeight: "600",
+              borderRadius: "10px",
+              "&:hover": {
+                color: "black",
+              },
+            }}
+          >
+            Sign Up
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
--- src/component/HomePage/HomePage.jsx
+++ src/component/HomePage/HomePage.jsx
@@ -4,21 +4,70 @@ import ImageSlide from "./ImageSlide";
 import Specialties from "./Specialties";
 import img4 from "../../assets/images/4.jpeg";
 import { useEffect, useState } from "react";
-import { getSpecialtiesPagination } from '../../services/specialtiesService';
-import { getAllDoctor } from '../../services/doctorService';
+import { getSpecialtiesPagination } from "../../services/specialtiesService";
+import { getAllDoctor } from "../../services/doctorService";
 import { getAllClinic } from "../../services/clinicService";
-import { Button, Card, CardContent, Typography, Grid, Container } from '@mui/material';
-import { Phone } from '@mui/icons-material';
-
+import {
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  Grid,
+  Container,
+} from "@mui/material";
+import { Phone } from "@mui/icons-material";
+import HomePage1 from "../../assets/images/Homepage-1.png";
+import PhoneInTalkOutlinedIcon from "@mui/icons-material/PhoneInTalkOutlined";
 
 const HomePage = (props) => {
-    return (
-        <div>
-            HomePage
+  return (
+    <div className="flex px-48 mx-10 mt-10  border-solid border-2">
+      <div className="w-3/5 flex flex-col gap-4">
+        <div className="font-serif text-6xl font-medium">
+          Sức khỏe răng miệng của bạn lịch hẹn chỉ cách một chạm!
         </div>
-    )
-}
-
-
+        <div className="w-3/4 font-roboto font-light ">
+          Chúng tôi chỉ sử dụng những vật liệu chất lượng cao nhất để mang đến
+          sự chăm sóc tốt nhất cho bệnh nhân. Vì vậy, hãy yên tâm và đặt lịch
+          hẹn ngay hôm nay
+        </div>
+        <div className="mt-5 flex ">
+          <Button
+            variant="outlined"
+            href="#outlined-buttons"
+            sx={{
+              color: "white",
+              backgroundColor: "#4C99FF",
+              padding: "0px 25px",
+              fontSize: "14px",
+              fontFamily: "Roboto Slab, serif",
+              fontWeight: "600",
+              borderRadius: "10px",
+              "&:hover": {
+                color: "black",
+              },
+            }}
+          >
+            Đặt lịch ngay
+          </Button>
+          <div className="flex">
+            <PhoneInTalkOutlinedIcon
+              sx={{
+                border: "solid 1px",
+              }}
+            />
+            <div>
+              <div>Dental 24H Emergency</div>
+              <div>0900-78601</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img className="w-96" src={HomePage1} />
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
