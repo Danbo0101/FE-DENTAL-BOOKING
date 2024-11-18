001ad1b4ab4e4d7f85abee3fc00e92837c784c9c
Danbo0101 <p.h.n.phuong0101@gmail.com>
2024-11-18 16:42:36 +0700

    doctor

--- src/component/User/Doctor/Doctor.jsx
+++ src/component/User/Doctor/Doctor.jsx
@@ -1,94 +1,188 @@
-import { useEffect, useState } from "react";
-import Breadcrumb from "../../General/Breadcrumb";
-import imgtest from "../../../assets/images/Untitled.png"
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Pagination from '@mui/material/Pagination';
-import { getAllDoctor } from "../../../services/doctorService";
-
-
+import Button from '@mui/material/Button';
+import imgdoctortest from "../../../assets/images/doctor1.png";
 
 
 const ListDoctor = (props) => {
 
-    const navigate = useNavigate();
-
-    const [indexBreadcrumb, setIndexBreadcrumb] = useState(1);
-    const [listDoctor, setListDoctor] = useState([]);
-
+    const listDoctor = [
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        },
+        {
+            image: imgdoctortest,
+            name: "Dr. Hùng Phúc",
+            specialty: "Chuyên khoa răng miệng",
+            schedule: "Mon - Sun"
+        }
+    ]
 
     const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 4;
+    const itemsPerPage = 5;
 
     const pageCount = Math.ceil(listDoctor.length / itemsPerPage);
     const currentData = listDoctor.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
 
-
-    const handleDoctorInfo = (id) => {
-        navigate(`/doctor-info/${id}`);
-    }
-
-    useEffect(() => {
-        const fetchDataDoctor = async () => {
-            let result = await getAllDoctor();
-            if (result.ER === 0) {
-                setListDoctor(result.data);
-            }
-        };
-
-        fetchDataDoctor();
-    }, []);
-
-    const bufferToDataURL = (buffer) => {
-        const blob = new Blob([new Uint8Array(buffer.data)], { type: 'image/jpeg' });
-        const url = URL.createObjectURL(blob);
-        return url;
-    }
-
     return (
-        <div className="py-10 px-32">
-            <div >
-                <Breadcrumb
-                    indexBreadcrumb={indexBreadcrumb}
-                />
+        <div className="flex flex-col">
+            <div className="w-full flex flex-col justify-center items-center">
+                <div className="text-center font-serif font-bold text-4xl mt-20">
+                    Khám Tổng Quát
+                </div>
+                <div className="w-2/5 text-center font-light text-sm mt-3">
+                    Khám, chẩn đoán và điều trị các vấn đề răng miệng cơ bản, đồng thời tư vấn về cách chăm sóc răng miệng.
+                </div>
             </div>
-            <div className="text-xl font-serif font-bold pt-10">
-                Danh sách bác sĩ
-            </div>
-            {
-                currentData && currentData.length > 0 ?
+            <div className="my-10">
+                {currentData && currentData.length > 0 ?
                     currentData.map((doctor, index) => {
                         return (
-                            <>
-                                <div className="flex pt-10 items-center cursor-pointer"
-                                    onClick={() => handleDoctorInfo(doctor.id)}
-                                >
-                                    <img src={bufferToDataURL(doctor.image)} className="w-36 h-28" />
-                                    <div className="flex flex-col ml-5 gap-1 ">
-                                        <a className="text-xl font-normal">
-                                            {doctor.qualification} {doctor.name}
-                                        </a>
-                                        <a className="text-base font-extralight">{doctor.specialty}</a>
-                                    </div>
+                            <div className="flex items-center mx-40 mt-10 border rounded-xl px-10 py-5 bg-white drop-shadow">
+                                <img src={doctor.image} className="w-28 h-28 rounded-full" />
+                                <div className="flex flex-col gap-1 ml-4">
+                                    <div className="text-xl font-semibold font-serif">{doctor.name}</div>
+                                    <div className="text-base font-extralight"> {doctor.specialty} </div>
+                                    <div className="text-base font-extralight ">Lịch Khám : {doctor.schedule}</div>
                                 </div>
-                                <hr />
-                            </>
+                                <div className="mt-16 ml-96">
+                                    <Button
+                                        variant="outlined"
+                                        href="#outlined-buttons"
+                                        sx={{
+                                            color: "white",
+                                            backgroundColor: "#4C99FF",
+                                            padding: "6px 25px",
+                                            fontSize: "10px",
+                                            fontFamily: "Roboto Slab, serif",
+                                            fontWeight: "600",
+                                            borderRadius: "10px",
+                                            marginLeft: "220px",
+                                            "&:hover": {
+                                                color: "black",
+                                            },
+                                        }}
+                                    >
+                                        Đặt lịch
+                                    </Button>
+                                </div>
+                            </div>
                         )
                     })
                     :
-                    <div className="text-center text-xl">Không có bác sĩ</div>
-            }
-            <div className=' flex items-center justify-center p-8'>
+                    <></>
+                }
+
+            </div>
+            <div className=' flex items-center justify-center'>
                 <Pagination
                     count={pageCount}
-                    variant="outlined"
                     color="primary"
                     page={currentPage}
                     onChange={(e, value) => setCurrentPage(value)}
                 />
             </div>
         </div>
-
-
     )
 }
 
