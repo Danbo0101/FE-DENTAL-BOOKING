d13f462589708e221498f7f4d55f265b9a2f47e1
Danbo0101 <danbo0101@gmail.com>
2024-11-25 10:09:04 -0500

    test

--- src/component/Admin/Doctor/Modal/CreateDoctor.jsx
+++ src/component/Admin/Doctor/Modal/CreateDoctor.jsx
@@ -1,257 +1,254 @@
-import { styled } from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
-import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
-import { useState } from 'react';
-import { toast } from 'react-toastify';
-import { postCreateNewDoctor } from '../../../../services/doctorService';
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { postCreateNewDoctor } from "../../../../services/doctorService";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-    '& .MuiDialogContent-root': {
-        padding: theme.spacing(2),
-    },
-    '& .MuiDialogActions-root': {
-        padding: theme.spacing(1),
-    },
+  "& .MuiDialogContent-root": {
+    padding: theme.spacing(2),
+  },
+  "& .MuiDialogActions-root": {
+    padding: theme.spacing(1),
+  },
 }));
 
 const CreateDoctor = (props) => {
-    const { open, pageCount } = props
+  const { open, pageCount } = props;
 
-    const handleClose = () => {
+  const handleClose = () => {
+    if (reason !== "backdropClick") {
+      onClose(event, reason);
+    }
+  };
 
-        if (reason !== 'backdropClick') {
-            onClose(event, reason);
-        }
+  const [fullName, setFullName] = useState("");
+  const [iD_Number, setID_Number] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
+  const [DOB, setDOB] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [image, setImage] = useState("");
+  const [nameImage, setNameImage] = useState("");
+  const [previewImage, setPreviewImage] = useState("");
 
-    };
+  const handleUploadImage = (event) => {
+    if (event.target && event.target.files && event.target.files[0]) {
+      setNameImage(event.target.files[0].name);
+      setPreviewImage(URL.createObjectURL(event.target.files[0]));
+      setImage(event.target.files[0]);
+    }
+  };
 
-    const [fullName, setFullName] = useState("");
-    const [iD_Number, setID_Number] = useState("");
-    const [phone, setPhone] = useState("");
-    const [email, setEmail] = useState("");
-    const [gender, setGender] = useState("");
-    const [DOB, setDOB] = useState("");
-    const [userName, setUserName] = useState("");
-    const [password, setPassword] = useState("");
-    const [image_Url, setImage_Url] = useState("");
-    const [nameImage, setNameImage] = useState("")
-    const [previewImage, setPreviewImage] = useState("");
+  const checkPhoneNumber = (phoneNumber) => {
+    phoneNumber = phoneNumber.replace(/[^\d]/g, "");
+    let PhonePattern = /^(0|(\+84))9\d{8}$/;
+    return PhonePattern.test(phoneNumber);
+  };
 
+  const resetData = () => {
+    setFullName("");
+    setID_Number("");
+    setPhone("");
+    setEmail("");
+    setGender("");
+    setDOB("");
+    setUserName("");
+    setPassword("");
+    setImage("");
+    setNameImage("");
+    setPreviewImage("");
+    props.setOpen(false);
+  };
 
-    const handleUploadImage = (event) => {
-        if (event.target && event.target.files && event.target.files[0]) {
-            setNameImage(event.target.files[0].name);
-            setPreviewImage(URL.createObjectURL(event.target.files[0]))
-            setImage_Url(event.target.files[0])
-        }
+  const handleSubmitCreate = async () => {
+    console.log(image_Url);
+
+    if (!fullName) {
+      toast.warn("Vui lòng nhập Tên");
+      return;
+    } else if (!email) {
+      console.log(1);
+      toast.warn("Vui lòng nhập Email");
+      return;
+    } else if (!userName) {
+      toast.warn("Vui lòng nhập Tên đăng nhập");
+      return;
+    } else if (!password) {
+      toast.warn("Vui lòng nhập mật khẩu");
+      return;
+    } else if (!DOB) {
+      toast.warn("Vui lòng chọn ngày sinh");
+      return;
+    } else if (!gender) {
+      toast.warn("Vui lòng chọn giới tính");
+      return;
+    } else if (!checkPhoneNumber(phone)) {
+      toast.warn("Số điện thoại không hợp lệ");
+      return;
+    } else if (!phone) {
+      toast.warn("Vui lòng nhập số điện thoại");
+      return;
+    } else if (!iD_Number) {
+      toast.warn("Vui lòng nhập CCCD");
+      return;
+    } else if (!image) {
+      toast.warn("Vui lòng upload ảnh");
+      return;
     }
 
-    const checkPhoneNumber = (phoneNumber) => {
-        phoneNumber = phoneNumber.replace(/[^\d]/g, '');
-        let PhonePattern = /^(0|(\+84))9\d{8}$/;
-        return PhonePattern.test(phoneNumber);
+    let result = await postCreateNewDoctor(
+      name,
+      email,
+      password,
+      address,
+      gender,
+      phone,
+      qualification,
+      price,
+      image
+    );
+    if (result.ER === 0) {
+      toast.success("Thêm Bác Sĩ Thành Công");
+      props.fetchListDoctor();
+      props.setCurrentPage(pageCount);
+      resetData();
+    } else {
+      toast.error(result.message);
     }
+  };
 
-    const resetData = () => {
-        setFullName("");
-        setID_Number("");
-        setPhone("");
-        setEmail("");
-        setGender("");
-        setDOB("");
-        setUserName("");
-        setPassword("");
-        setImage_Url("");
-        setNameImage("");
-        setPreviewImage("");
-        props.setOpen(false);
-    }
-
-    const handleSubmitCreate = async () => {
-
-        if (!fullName) {
-            toast.warn("Vui lòng nhập Tên")
-            return;
-        }
-        else if (!email) {
-            console.log(1)
-            toast.warn("Vui lòng nhập Email")
-            return;
-        }
-        else if (!userName) {
-            toast.warn("Vui lòng nhập Tên đăng nhập")
-            return;
-        }
-        else if (!password) {
-            toast.warn("Vui lòng nhập mật khẩu")
-            return;
-        }
-        else if (!DOB) {
-            toast.warn("Vui lòng chọn ngày sinh")
-            return;
-        }
-        else if (!gender) {
-            toast.warn("Vui lòng chọn giới tính")
-            return;
-        }
-        else if (!checkPhoneNumber(phone)) {
-            toast.warn("Số điện thoại không hợp lệ")
-            return;
-        }
-        else if (!phone) {
-            toast.warn("Vui lòng nhập số điện thoại")
-            return;
-        }
-        else if (!iD_Number) {
-            toast.warn("Vui lòng nhập CCCD")
-            return;
-        }
-
-        else if (!image_Url) {
-            toast.warn("Vui lòng upload ảnh")
-            return;
-        }
-
-        // let result = await postCreateNewDoctor(name, email, password, address, gender, phone, qualification, price, image);
-        // if (result.ER === 0) {
-        //     toast.success("Thêm Bác Sĩ Thành Công");
-        //     props.fetchListDoctor();
-        //     props.setCurrentPage(pageCount);
-        //     resetData();
-
-        // }
-        // else {
-        //     toast.error(result.message)
-        // }
-    }
-
-    return (
-        <BootstrapDialog
-            onClose={handleClose}
-            aria-labelledby="customized-dialog-title"
-            open={open}
-            maxWidth="md"
-            fullWidth
+  return (
+    <BootstrapDialog
+      onClose={handleClose}
+      aria-labelledby="customized-dialog-title"
+      open={open}
+      maxWidth="md"
+      fullWidth
+    >
+      <DialogTitle
+        sx={{ m: 0, p: 2, fontSize: "20px", fontWeight: "500" }}
+        id="customized-dialog-title"
+      >
+        Tạo mới Bác sĩ
+      </DialogTitle>
+      <DialogContent dividers>
+        <div className="mx-10 flex flex-col gap-5">
+          <input
+            className="w-full px-8 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+            type="text"
+            placeholder="Họ và Tên"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+          />
+          <input
+            className="w-full px-8 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="w-full px-8 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+            type="text"
+            placeholder="Tên Đăng Nhập"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <input
+            className="w-full px-8 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+            type="password"
+            placeholder="Mật khẩu"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="flex gap-2">
+            <input
+              type="date"
+              value={DOB}
+              onChange={(e) => setDOB(e.target.value)}
+              className="block custom-select px-8 py-3 w-1/2 mt-1 bg-gray-100 border border-gray-100 font-medium text-sm text-gray-500  rounded-lg shadow-sm focus:border-gray-400 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            />
+            <select
+              id="gender"
+              name="gender"
+              value={gender}
+              onChange={(e) => setGender(e.target.value)}
+              className="block custom-select px-8 py-3 w-1/2 mt-1 bg-gray-100 border border-gray-100 font-medium text-sm text-gray-500  rounded-lg shadow-sm focus:border-gray-400 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            >
+              <option value="" disabled>
+                Giới tính
+              </option>
+              <option value="nam">Nam</option>
+              <option value="nu">Nữ</option>
+              <option value="khac">Khác</option>
+            </select>
+          </div>
+          <div className="flex gap-2">
+            <input
+              className="w-1/2 px-8 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-1"
+              type="text"
+              placeholder="Số điện thoại"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+            <input
+              className="w-1/2 px-8 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-1"
+              type="text"
+              placeholder="CCCD"
+              value={iD_Number}
+              onChange={(e) => setID_Number(e.target.value)}
+            />
+          </div>
+          <div className="flex item-center text-sm">
+            <label className='"form-label label-upload' htmlFor="labelUpload">
+              <AddPhotoAlternateIcon
+                sx={{
+                  fontSize: "30px",
+                  color: "deepskyblue",
+                  cursor: "pointer",
+                }}
+              />
+              {nameImage ? nameImage : "Thêm hình ảnh"}
+            </label>
+            <input
+              type="file"
+              id="labelUpload"
+              hidden
+              onChange={(event) => handleUploadImage(event)}
+            />
+          </div>
+          <div className="w-full flex justify-center outline-dotted outline-slate-200">
+            {previewImage ? (
+              <img src={previewImage} className="w-32 h-32 p-2" />
+            ) : (
+              <></>
+            )}
+            {/* <span>Preview Image</span> */}
+          </div>
+        </div>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          variant="outlined"
+          color="success"
+          onClick={() => handleSubmitCreate()}
         >
-            <DialogTitle sx={{ m: 0, p: 2, fontSize: "20px", fontWeight: "500" }} id="customized-dialog-title">
-                Tạo mới Bác sĩ
-            </DialogTitle>
-            <DialogContent dividers>
-                <div className="mx-10 flex flex-col gap-5">
-                    <input
-                        className="w-full px-8 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                        type="text"
-                        placeholder="Họ và Tên"
-                        value={fullName}
-                        onChange={(e) => setFullName(e.target.value)}
-                    />
-                    <input
-                        className="w-full px-8 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                        type="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <input
-                        className="w-full px-8 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                        type="text"
-                        placeholder="Tên Đăng Nhập"
-                        value={userName}
-                        onChange={(e) => setUserName(e.target.value)}
-                    />
-                    <input
-                        className="w-full px-8 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
-                        type="password"
-                        placeholder="Mật khẩu"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <div className="flex gap-2">
-                        <input
-                            type="date"
-                            value={DOB}
-                            onChange={(e) => setDOB(e.target.value)}
-                            className="block custom-select px-8 py-3 w-1/2 mt-1 bg-gray-100 border border-gray-100 font-medium text-sm text-gray-500  rounded-lg shadow-sm focus:border-gray-400 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                        />
-                        <select
-                            id="gender"
-                            name="gender"
-                            value={gender}
-                            onChange={(e) => setGender(e.target.value)}
-                            className="block custom-select px-8 py-3 w-1/2 mt-1 bg-gray-100 border border-gray-100 font-medium text-sm text-gray-500  rounded-lg shadow-sm focus:border-gray-400 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                        >
-                            <option value="" disabled>Giới tính</option>
-                            <option value="nam">Nam</option>
-                            <option value="nu">Nữ</option>
-                            <option value="khac">Khác</option>
-                        </select>
-                    </div>
-                    <div className="flex gap-2">
-                        <input
-                            className="w-1/2 px-8 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-1"
-                            type="text"
-                            placeholder="Số điện thoại"
-                            value={phone}
-                            onChange={(e) => setPhone(e.target.value)}
-                        />
-                        <input
-                            className="w-1/2 px-8 py-3 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-1"
-                            type="text"
-                            placeholder="CCCD"
-                            value={iD_Number}
-                            onChange={(e) => setID_Number(e.target.value)}
-                        />
-                    </div>
-                    <div className='flex item-center text-sm' >
-                        <label className='"form-label label-upload' htmlFor='labelUpload'>
-                            <AddPhotoAlternateIcon
-                                sx={{ fontSize: "30px", color: "deepskyblue", cursor: "pointer" }}
-                            />
-                            {nameImage ? nameImage : "Thêm hình ảnh"}
-
-                        </label>
-                        <input
-                            type='file'
-                            id='labelUpload'
-                            hidden
-                            onChange={(event) => handleUploadImage(event)}
-                        />
-                    </div>
-                    <div className='w-full flex justify-center outline-dotted outline-slate-200'>
-                        {previewImage ?
-                            <img src={previewImage} className="w-32 h-32 p-2" />
-                            :
-                            <></>
-                        }
-                        {/* <span>Preview Image</span> */}
-
-                    </div>
-                </div>
-            </DialogContent>
-            <DialogActions>
-                <Button
-                    variant="outlined"
-                    color='success'
-                    onClick={() => handleSubmitCreate()}
-                >
-                    Tạo mới
-                </Button>
-                <Button
-                    variant="outlined"
-                    color='inherit'
-                    onClick={() => resetData()}
-                >
-                    Đóng
-                </Button>
-
-            </DialogActions>
-        </BootstrapDialog>
-    )
-}
+          Tạo mới
+        </Button>
+        <Button variant="outlined" color="inherit" onClick={() => resetData()}>
+          Đóng
+        </Button>
+      </DialogActions>
+    </BootstrapDialog>
+  );
+};
 
 export default CreateDoctor;
-
--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -1,82 +1,101 @@
 import axios from "../utils/axiosCustomize";
 
-
 const getDoctorPagination = (page, limit) => {
-    return axios.get(`/v1/api/doctors?page=${page}&limit=${limit}`);
-}
+  return axios.get(`/v1/api/doctors?page=${page}&limit=${limit}`);
+};
 
 const getAllDoctor = () => {
-    return axios.get(`/v1/api/doctors`);
-}
+  return axios.get(`/v1/api/doctors`);
+};
 
 const getAllDoctorNotMarkToday = () => {
-    return axios.get(`/v1/api/mark-attendance`);
-}
+  return axios.get(`/v1/api/mark-attendance`);
+};
 
 const getDoctorInfoDetail = (id) => {
-    return axios.get(`/v1/api/doctor-clinic-specialties?doctorId=${id}`);
-}
+  return axios.get(`/v1/api/doctor-clinic-specialties?doctorId=${id}`);
+};
 
-const postCreateNewDoctor = (name, email, password, address, gender, phone, qualification, price, doctorImage) => {
-    const data = new FormData();
-    data.append('name', name);
-    data.append('email', email);
-    data.append('password', password);
-    data.append('address', address);
-    data.append('gender', gender);
-    data.append('phone', phone);
-    data.append('qualification', qualification);
-    data.append('price', price);
-    data.append('image', doctorImage);
-    return axios.post('/v1/api/doctors', data);
-}
+const postCreateNewDoctor = (
+  fullName,
+  iD_Number,
+  phone,
+  email,
+  gender,
+  DOB,
+  userName,
+  password,
+  image
+) => {
+  const data = new FormData();
+  data.append("fullName", fullName);
+  data.append("iD_Number", iD_Number);
+  data.append("phone", phone);
+  data.append("email", email);
+  data.append("gender", gender);
+  data.append("DOB", DOB);
+  data.append("userName", userName);
+  data.append("password", password);
+  data.append("image_Url", image);
+  role_Id;
+  return axios.post("/api/User/create", data);
+};
 
 const postAssignDoctor = (doctorId, clinicId, specialtiesId) => {
-    const data = new FormData();
-    data.append('doctorId', doctorId);
-    data.append('clinicId', clinicId);
-    data.append('specialtiesId', specialtiesId);
-    return axios.post('/v1/api/assign-doctor', data);
-}
+  const data = new FormData();
+  data.append("doctorId", doctorId);
+  data.append("clinicId", clinicId);
+  data.append("specialtiesId", specialtiesId);
+  return axios.post("/v1/api/assign-doctor", data);
+};
 
 const deleteDoctor = (id) => {
-    return axios.delete(`/v1/api/doctors?id=${id}`);
-}
+  return axios.delete(`/v1/api/doctors?id=${id}`);
+};
 
-const putUpdateDoctor = (id, name, email, address, gender, phone, qualification, price, doctorImage) => {
-    const data = new FormData();
-    data.append('name', name);
-    data.append('email', email);
-    data.append('address', address);
-    data.append('gender', gender);
-    data.append('phone', phone);
-    data.append('qualification', qualification);
-    data.append('price', price);
-    data.append('image', doctorImage);
-    return axios.put(`/v1/api/doctors?id=${id}`, data);
-}
+const putUpdateDoctor = (
+  id,
+  name,
+  email,
+  address,
+  gender,
+  phone,
+  qualification,
+  price,
+  doctorImage
+) => {
+  const data = new FormData();
+  data.append("name", name);
+  data.append("email", email);
+  data.append("address", address);
+  data.append("gender", gender);
+  data.append("phone", phone);
+  data.append("qualification", qualification);
+  data.append("price", price);
+  data.append("image", doctorImage);
+  return axios.put(`/v1/api/doctors?id=${id}`, data);
+};
 
 const getAssignDoctor = (id) => {
-    return axios.get(`/v1/api/assign-doctor?doctorId=${id}`);
-}
+  return axios.get(`/v1/api/assign-doctor?doctorId=${id}`);
+};
 
 const putUpdateAssignDoctor = (id, clinicId, specialtiesId) => {
-    const data = new FormData();
-    data.append('clinicId', clinicId);
-    data.append('specialtiesId', specialtiesId);
-    return axios.put(`/v1/api/assign-doctor?doctorId=${id}`, data);
-}
-
+  const data = new FormData();
+  data.append("clinicId", clinicId);
+  data.append("specialtiesId", specialtiesId);
+  return axios.put(`/v1/api/assign-doctor?doctorId=${id}`, data);
+};
 
 export {
-    getAllDoctor,
-    getDoctorPagination,
-    postCreateNewDoctor,
-    getDoctorInfoDetail,
-    postAssignDoctor,
-    deleteDoctor,
-    putUpdateDoctor,
-    getAssignDoctor,
-    putUpdateAssignDoctor,
-    getAllDoctorNotMarkToday
-}
+  getAllDoctor,
+  getDoctorPagination,
+  postCreateNewDoctor,
+  getDoctorInfoDetail,
+  postAssignDoctor,
+  deleteDoctor,
+  putUpdateDoctor,
+  getAssignDoctor,
+  putUpdateAssignDoctor,
+  getAllDoctorNotMarkToday,
+};
