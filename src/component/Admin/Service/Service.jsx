f6449b19a80acbce889061df39c9e6dcf4bdf8c2
Danbo0101 <danbo0101@gmail.com>
2024-11-28 11:12:51 -0500

    final-1

--- src/component/Admin/Service/Service.jsx
+++ src/component/Admin/Service/Service.jsx
@@ -1,198 +1,210 @@
-import LocalHospitalOutlinedIcon from '@mui/icons-material/LocalHospitalOutlined';
-import { styled } from '@mui/material/styles';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import UpdateOutlinedIcon from '@mui/icons-material/UpdateOutlined';
-import { useEffect, useState } from 'react';
-import Pagination from '@mui/material/Pagination';
-import { getServices, getSpecialtiesPagination } from '../../../services/specialtiesService';
-import CreateSpecialties from './Modal/CreateSpecialties';
-import SpecialtiesInfo from './Modal/SpecialtiesInfo';
-import UpdateSpecialties from './Modal/UpdateClinic';
-import DeleteSpecialties from './Modal/DeleteSpecialties';
-import CreatService from './Modal/CreateSpecialties';
-
+import LocalHospitalOutlinedIcon from "@mui/icons-material/LocalHospitalOutlined";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import UpdateOutlinedIcon from "@mui/icons-material/UpdateOutlined";
+import { useEffect, useState } from "react";
+import Pagination from "@mui/material/Pagination";
+import {
+  getServices,
+  getSpecialtiesPagination,
+} from "../../../services/specialtiesService";
+import SpecialtiesInfo from "./Modal/ServiceInfo";
+import UpdateSpecialties from "./Modal/UpdateClinic";
+import DeleteSpecialties from "./Modal/DeleteSpecialties";
+import CreatService from "./Modal/CreateService";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
-    [`&.${tableCellClasses.head}`]: {
-        backgroundColor: "#C0C0C0",
-        color: theme.palette.common.black,
-        fontSize: 15,
-        fontWeight: 700,
-    },
-    [`&.${tableCellClasses.body}`]: {
-        fontSize: 14,
-    },
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#C0C0C0",
+    color: theme.palette.common.black,
+    fontSize: 15,
+    fontWeight: 700,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
 }));
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
-    '&:nth-of-type(odd)': {
-        backgroundColor: theme.palette.action.hover,
-
-    },
-    // hide last border
-    '&:last-child td, &:last-child th': {
-        border: 0,
-    },
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
 }));
 
 const Service = (props) => {
+  const [listService, setListService] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
 
-    const [listService, setListService] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const itemsPerPage = 4;
+  const [openCreate, setOpenCreate] = useState(false);
 
-    const [openCreate, setOpenCreate] = useState(false);
+  // const [openView, setOpenView] = useState(false);
+  // const [dataView, setDataView] = useState();
 
-    // const [openView, setOpenView] = useState(false);
-    // const [dataView, setDataView] = useState();
+  // const [openUpdate, setOpenUpdate] = useState(false);
+  // const [dataUpdate, setDataUpdate] = useState();
 
-    // const [openUpdate, setOpenUpdate] = useState(false);
-    // const [dataUpdate, setDataUpdate] = useState();
+  // const [openDelete, setOpenDelete] = useState(false);
+  // const [dataDelete, setDataDelete] = useState();
 
-    // const [openDelete, setOpenDelete] = useState(false);
-    // const [dataDelete, setDataDelete] = useState();
+  useEffect(() => {
+    fetchListService();
+  }, [currentPage]);
 
-    useEffect(() => {
-        fetchListService();
-    }, [currentPage])
-
-    const fetchListService = async () => {
-
-        let result = await getServices();
-        if (result.success) {
-            setListService(result.data);
-            return;
-        }
-        else {
-            console.log(result.message)
-        }
-
+  const fetchListService = async () => {
+    let result = await getServices();
+    if (result.success) {
+      setListService(result.data);
+      return;
+    } else {
+      console.log(result.message);
     }
+  };
 
+  const pageCount = Math.ceil(listService.length / itemsPerPage);
+  const currentData = listService.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
 
-    const pageCount = Math.ceil(listService.length / itemsPerPage);
-    const currentData = listService.slice(
-        (currentPage - 1) * itemsPerPage,
-        currentPage * itemsPerPage
-    );
+  // const handleViewSpecialties = (id, name, description, image) => {
+  //     setOpenView(true);
+  //     setDataView({
+  //         id,
+  //         name,
+  //         description,
+  //         image
+  //     });
+  // }
 
+  // const handleUpdateSpecialties = (id, name, description, image) => {
+  //     setOpenUpdate(true);
+  //     setDataUpdate({
+  //         id,
+  //         name,
+  //         description,
+  //         image
+  //     });
+  // }
 
+  // const handleDelteSpecialties = (id, name) => {
+  //     setOpenDelete(true);
+  //     setDataDelete({
+  //         id,
+  //         name,
 
-    // const handleViewSpecialties = (id, name, description, image) => {
-    //     setOpenView(true);
-    //     setDataView({
-    //         id,
-    //         name,
-    //         description,
-    //         image
-    //     });
-    // }
+  //     });
+  // }
 
-    // const handleUpdateSpecialties = (id, name, description, image) => {
-    //     setOpenUpdate(true);
-    //     setDataUpdate({
-    //         id,
-    //         name,
-    //         description,
-    //         image
-    //     });
-    // }
+  return (
+    <div className="flex flex-col w-full h-full py-10 px-16">
+      <div className="flex justify-between items-center text-2xl font-semibold pb-5">
+        Quản lý Dịch Vụ
+        <div onClick={() => setOpenCreate(true)}>
+          <LocalHospitalOutlinedIcon
+            sx={{ fontSize: "30px", color: "limegreen", cursor: "pointer" }}
+          />
+        </div>
+      </div>
+      <hr className="my-3 border-t" />
 
-    // const handleDelteSpecialties = (id, name) => {
-    //     setOpenDelete(true);
-    //     setDataDelete({
-    //         id,
-    //         name,
+      <div className="pt-5">
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell align="center">ID</StyledTableCell>
+                <StyledTableCell align="center">Name</StyledTableCell>
+                <StyledTableCell align="center">Action</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {currentData &&
+                currentData.length > 0 &&
+                currentData.map((service, index) => {
+                  return (
+                    <StyledTableRow
+                      key={service.service_Id}
+                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                    >
+                      <StyledTableCell
+                        component="th"
+                        scope="row"
+                        align="center"
+                      >
+                        {index}
+                      </StyledTableCell>
+                      <StyledTableCell align="center">
+                        {service.name}
+                      </StyledTableCell>
+                      <StyledTableCell align="center">
+                        <IconButton
+                          aria-label="info"
+                          color="info"
+                          onClick={() =>
+                            handleViewSpecialties(
+                              specialties.id,
+                              specialties.name,
+                              specialties.description,
+                              specialties.image
+                            )
+                          }
+                        >
+                          <InfoOutlinedIcon />
+                        </IconButton>
+                        <IconButton
+                          aria-label="update"
+                          color="warning"
+                          // onClick={() => handleUpdateSpecialties(specialties.id, specialties.name, specialties.description, specialties.image)}
+                        >
+                          <UpdateOutlinedIcon />
+                        </IconButton>
+                        <IconButton
+                          aria-label="delete"
+                          color="error"
+                          // onClick={() => handleDelteSpecialties(specialties.id, specialties.name)}
+                        >
+                          <DeleteIcon />
+                        </IconButton>
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <div className=" flex items-center justify-center p-8">
+          <Pagination
+            count={pageCount}
+            variant="outlined"
+            color="primary"
+            page={currentPage}
+            onChange={(e, value) => setCurrentPage(value)}
+          />
+        </div>
+      </div>
 
-    //     });
-    // }
-
-    return (
-        <div className="flex flex-col w-full h-full py-10 px-16">
-            <div className='flex justify-between items-center text-2xl font-semibold pb-5'>
-                Quản lý Dịch Vụ
-                <div onClick={() => setOpenCreate(true)}>
-                    <LocalHospitalOutlinedIcon sx={{ fontSize: "30px", color: "limegreen", cursor: "pointer" }} />
-                </div>
-            </div>
-            <hr className="my-3 border-t" />
-
-            <div className='pt-5'>
-                <TableContainer component={Paper}>
-                    <Table sx={{ minWidth: 650 }} aria-label="customized table">
-                        <TableHead>
-                            <TableRow>
-                                <StyledTableCell align='center'>ID</StyledTableCell>
-                                <StyledTableCell align="center">Name</StyledTableCell>
-                                <StyledTableCell align="center">Action</StyledTableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {currentData && currentData.length > 0 &&
-                                currentData.map((service, index) => {
-                                    return (
-                                        <StyledTableRow
-                                            key={service.service_Id}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                                        >
-                                            <StyledTableCell component="th" scope="row" align='center'>
-                                                {index}
-                                            </StyledTableCell>
-                                            <StyledTableCell align="center">{service.name}</StyledTableCell>
-                                            <StyledTableCell align="center">
-                                                <IconButton aria-label="info" color='info'
-                                                    onClick={() => handleViewSpecialties(specialties.id, specialties.name, specialties.description, specialties.image)}
-                                                >
-                                                    <InfoOutlinedIcon />
-                                                </IconButton>
-                                                <IconButton aria-label="update" color='warning'
-                                                // onClick={() => handleUpdateSpecialties(specialties.id, specialties.name, specialties.description, specialties.image)}
-                                                >
-                                                    <UpdateOutlinedIcon />
-                                                </IconButton>
-                                                <IconButton aria-label="delete" color='error'
-                                                // onClick={() => handleDelteSpecialties(specialties.id, specialties.name)}
-                                                >
-                                                    <DeleteIcon />
-                                                </IconButton>
-                                            </StyledTableCell>
-                                        </StyledTableRow>
-                                    )
-                                })
-                            }
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <div className=' flex items-center justify-center p-8'>
-                    <Pagination
-                        count={pageCount}
-                        variant="outlined"
-                        color="primary"
-                        page={currentPage}
-                        onChange={(e, value) => setCurrentPage(value)}
-                    />
-                </div>
-
-            </div>
-
-
-            <CreatService
-                open={openCreate}
-                setOpen={setOpenCreate}
-                fetchListService={fetchListService}
-                pageCount={pageCount}
-                setCurrentPage={setCurrentPage}
-            />
-            {/*  <SpecialtiesInfo
+      <CreatService
+        open={openCreate}
+        setOpen={setOpenCreate}
+        fetchListService={fetchListService}
+        pageCount={pageCount}
+        setCurrentPage={setCurrentPage}
+      />
+      {/*  <SpecialtiesInfo
                 open={openView}
                 setOpen={setOpenView}
                 dataView={dataView}
@@ -212,9 +224,8 @@ const Service = (props) => {
                 setDataDelete={setDataDelete}
                 fetchListSpecialties={fetchListSpecialties}
             /> */}
-
-        </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Service;
