8a2c92ff7f6e37e6e8c8bba9b1a8a4e06a8d2d3a
Danbo0101 <danbo0101@gmail.com>
2024-12-09 08:49:59 -0500

    register

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,82 +1,72 @@
-import axios from "../utils/axiosCustomize"
-
-
-
+import axios from "../utils/axiosCustomize";
 
 const postLogin = (userName, password) => {
-    const data = new FormData();
-    data.append('userName', userName);
-    data.append('password', password);
-    return axios.post(`/api/Auth/login`, data);
-}
-
-const postSendOTP = (email, type) => {
-    return axios.post(`/v1/api/sent-otp`, {
-        email,
-        type
-    });
-}
-
-const postVerifyOTP = (email, otp) => {
-    return axios.post(`/v1/api/verify-otp`, {
-        email,
-        otp
-    });
-}
+  const data = new FormData();
+  data.append("userName", userName);
+  data.append("password", password);
+  return axios.post(`/api/Auth/login`, data);
+};
 
 const postForgotPassword = (email, newPassword, confirmPassword) => {
-    return axios.post(`/v1/api/forgot-password`, {
-        email,
-        newPassword,
-        confirmPassword
-    });
-}
+  return axios.post(`/v1/api/forgot-password`, {
+    email,
+    newPassword,
+    confirmPassword,
+  });
+};
 
-
-const postRegister = (name, email, password, address, gender, phone, image) => {
-    const data = new FormData();
-    data.append('name', name);
-    data.append('email', email);
-    data.append('password', password);
-    data.append('address', address);
-    data.append('gender', gender);
-    data.append('phone', phone);
-    data.append('image', image);
-    return axios.post(`/v1/api/register`, data);
-}
-
+const postRegister = (
+  fullName,
+  iD_Number,
+  phone,
+  email,
+  gender,
+  birthday,
+  image,
+  username,
+  password
+) => {
+  const data = new FormData();
+  data.append("fullName", fullName);
+  data.append("iD_Number", iD_Number);
+  data.append("phone", phone);
+  data.append("email", email);
+  data.append("gender", gender);
+  data.append("birthday", birthday);
+  data.append("image", image);
+  data.append("username", username);
+  data.append("password", password);
+  return axios.post(`/api/Auth/signup`, data);
+};
 
 const postLogout = () => {
-    return axios.post(`/v1/api/logout`);
-}
+  return axios.post(`/v1/api/logout`);
+};
 
 const putChangeProfile = (id, name, email, address, gender, phone, image) => {
-    const data = new FormData();
-    data.append('name', name);
-    data.append('email', email);
-    data.append('address', address);
-    data.append('gender', gender);
-    data.append('phone', phone);
-    data.append('image', image);
-    return axios.put(`/v1/api/change-profile?id=${id}`, data);
-}
+  const data = new FormData();
+  data.append("name", name);
+  data.append("email", email);
+  data.append("address", address);
+  data.append("gender", gender);
+  data.append("phone", phone);
+  data.append("image", image);
+  return axios.put(`/v1/api/change-profile?id=${id}`, data);
+};
 
 const putChangePassword = (id, oldPassword, newPassword, confirmPassword) => {
-    const data = new FormData();
-    data.append('oldPassword', oldPassword);
-    data.append('newPassword', newPassword);
-    data.append('confirmPassword', confirmPassword);
-    return axios.put(`/v1/api/change-password?id=${id}`, data);
-}
+  const data = new FormData();
+  data.append("oldPassword", oldPassword);
+  data.append("newPassword", newPassword);
+  data.append("confirmPassword", confirmPassword);
+  return axios.put(`/v1/api/change-password?id=${id}`, data);
+};
 
 export {
-    postLogin,
-    postLogout,
-    putChangeProfile,
-    putChangePassword,
-    postSendOTP,
-    postVerifyOTP,
-    postRegister,
-    postForgotPassword,
-
-}
+  postLogin,
+  postLogout,
+  putChangeProfile,
+  putChangePassword,
+  postRegister,
+  postForgotPassword,
+};
